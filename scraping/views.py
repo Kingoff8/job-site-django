648e25f626b0b36b8f34295e6e2ce216523d32f3
Kingoff8 <a.kingoff@gmail.com>
2020-10-13 00:34:46 +0200

    Update

--- scraping/views.py
+++ scraping/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from .models import Vacancy
 
 def home_view(request):
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    
+    #if city or language:
+        
+
     sq = Vacancy.objects.all()
     return render(request, 'scraping/home.html', {'object_list': sq})
