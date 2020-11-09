552cedd1124374ee9fbeddd00786ed73b925766c
Kingoff8 <a.kingoff@gmail.com>
2020-11-09 13:01:05 +0200

    custom user

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import UserManager
-from django.contrib.auth.base_user import AbstractBaseUser 
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
 
 class MyUser(AbstractBaseUser):
     email = models.EmailField(
@@ -11,13 +45,17 @@ class MyUser(AbstractBaseUser):
     date_of_birth = models.DateField()
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    city = models.ForeignKey('City' , on_delete=models.SET_NULL, 
-    null=True, 
-    blank=True)
+    city = models.ForeignKey('scraping.City' , on_delete=models.SET_NULL, 
+    null=True, blank=True)
 
-    objects = UserManager()
+    language = models.ForeignKey('scraping.Language' , on_delete=models.SET_NULL, 
+    null=True, blank=True)
+
+
+    objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
+
     REQUIRED_FIELDS = ['date_of_birth']
 
     def __str__(self):
@@ -38,3 +76,7 @@ class MyUser(AbstractBaseUser):
         "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
         return self.is_admin
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
