3400555d6be08a1ccc3dbad7cee89847bd371a51
Kingoff8 <a.kingoff@gmail.com>
2021-02-14 02:25:07 +0200

    Перевод парсера hh_ru на pytest

--- scraping/pytest_folder/pages/locators.py
+++ scraping/pytest_folder/pages/locators.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 
+
 class HhRuLocators:
-    START_LINK = 'https://юзерагент.рф'
+
+    START_LINK = 'https://hh.ru/search/vacancy?st=searchVacancy&text=&specialization=1&area=1020&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=5&no_magic=true&L_save_area=true'
+    LIST_LINKS = (By.CSS_SELECTOR, 'a[data-qa=vacancy-serp__vacancy-title]')
     TITLE = (By.TAG_NAME, 'h2')
-#    TITLE = ".find_elements_by_tag_name('h2')"
--- scraping/pytest_folder/test_hh_ru.py
+++ scraping/pytest_folder/test_hh_ru.py
@@ -1,12 +1,19 @@
 import time
 from pages.locators import HhRuLocators
+from pages.hh_ru import HhRuScrap
 
 
 def test_hh_ru(driver):
 
-    #link = 'https://юзерагент.рф'
-    driver.get(HhRuLocators.START_LINK)
+    link = HhRuLocators.START_LINK
+    page = HhRuScrap(driver, link)
+    page.open()
+    page.pars_vacancy()
+    # list_links = page.get_list()
+    # page.extract_vacancy(driver, list_links)
+
+    #driver.get(HhRuLocators.START_LINK)
     time.sleep(1)
-    print(driver.page_source)
-    title = driver.find_element(*HhRuLocators.TITLE).text
-    print(title)
+    #print(driver.page_source)
+    
+    #print(title)
